aac0c20296e3a18cc2d565ce076065c7a8d3c4e6
Harshad Dhumane <64719740+harshad-Dhumane@users.noreply.github.com>
2023-05-16 11:40:28 +0530

    Update bfs.cpp

--- bfs.cpp
+++ bfs.cpp
@@ -1,144 +1,113 @@
-include<iostream>
+#include<iostream>
 #include<stdlib.h>
 #include<queue>
 using namespace std;
 
-
 class node
 {
-   public:
-    
+public:
     node *left, *right;
     int data;
-
-};    
+};
 
 class Breadthfs
 {
- 
- public:
- 
- node *insert(node *, int);
- void bfs(node *);
- 
+public:
+    node *insert(node *, int);
+    void bfs(node *);
 };
 
-
-node *insert(node *root, int data)
-// inserts a node in tree
+node *Breadthfs::insert(node *root, int data)
 {
-
-    if(!root)
+    if (!root)
     {
-   	 
-   	 root=new node;
-   	 root->left=NULL;
-   	 root->right=NULL;
-   	 root->data=data;
-   	 return root;
+        root = new node;
+        root->left = NULL;
+        root->right = NULL;
+        root->data = data;
+        return root;
     }
 
     queue<node *> q;
     q.push(root);
-    
-    while(!q.empty())
+
+    while (!q.empty())
     {
+        node *temp = q.front();
+        q.pop();
 
-   	 node *temp=q.front();
-   	 q.pop();
-    
-   	 if(temp->left==NULL)
-   	 {
-   		 
-   		 temp->left=new node;
-   		 temp->left->left=NULL;
-   		 temp->left->right=NULL;
-   		 temp->left->data=data;    
-   		 return root;
-   	 }
-   	 else
-   	 {
+        if (temp->left == NULL)
+        {
+            temp->left = new node;
+            temp->left->left = NULL;
+            temp->left->right = NULL;
+            temp->left->data = data;
+            return root;
+        }
+        else
+        {
+            q.push(temp->left);
+        }
 
-   	 q.push(temp->left);
-
-   	 }
-
-   	 if(temp->right==NULL)
-   	 {
-   		 
-   		 temp->right=new node;
-   		 temp->right->left=NULL;
-   		 temp->right->right=NULL;
-   		 temp->right->data=data;    
-   		 return root;
-   	 }
-   	 else
-   	 {
-
-   	 q.push(temp->right);
-
-   	 }
-
+        if (temp->right == NULL)
+        {
+            temp->right = new node;
+            temp->right->left = NULL;
+            temp->right->right = NULL;
+            temp->right->data = data;
+            return root;
+        }
+        else
+        {
+            q.push(temp->right);
+        }
     }
-    
 }
 
-
-void bfs(node *head)
+void Breadthfs::bfs(node *head)
 {
+    queue<node *> q;
+    q.push(head);
 
-   	 queue<node*> q;
-   	 q.push(head);
-   	 
-   	 int qSize;
-   	 
-   	 while (!q.empty())
-   	 {
-   		 qSize = q.size();
-   		 #pragma omp parallel for
-            	//creates parallel threads
-   		 for (int i = 0; i < qSize; i++)
-   		 {
-   			 node* currNode;
-   			 #pragma omp critical
-   			 {
-   			   currNode = q.front();
-   			   q.pop();
-   			   cout<<"\t"<<currNode->data;
-   			   
-   			 }// prints parent node
-   			 #pragma omp critical
-   			 {
-   			 if(currNode->left)// push parent's left node in queue
-   				 q.push(currNode->left);
-   			 if(currNode->right)
-   				 q.push(currNode->right);
-   			 }// push parent's right node in queue   	 
+    while (!q.empty())
+    {
+        int qSize = q.size();
 
-   		 }
-   	 }
+        for (int i = 0; i < qSize; i++)
+        {
+            node *currNode = q.front();
+            q.pop();
+            cout << "\t" << currNode->data;
 
+            if (currNode->left)
+                q.push(currNode->left);
+            if (currNode->right)
+                q.push(currNode->right);
+        }
+    }
 }
 
-int main(){
-
-    node *root=NULL;
+int main()
+{
+    node *root = NULL;
     int data;
     char ans;
-    
+
     do
     {
-   	 cout<<"\n enter data=>";
-   	 cin>>data;
-   	 
-   	 root=insert(root,data);
-    
-   	 cout<<"do you want insert one more node?";
-   	 cin>>ans;
-    
-    }while(ans=='y'||ans=='Y');
-    
-    bfs(root);
-    
+        cout << "\nEnter data: ";
+        cin >> data;
+
+        Breadthfs obj;
+        root = obj.insert(root, data);
+
+        cout << "Do you want to insert one more node? (y/n): ";
+        cin >> ans;
+
+    } while (ans == 'y' || ans == 'Y');
+
+    Breadthfs obj;
+    obj.bfs(root);
+
     return 0;
 }
